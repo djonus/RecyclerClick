54cb3e5852c132bfa57189b658a265a0f8a324c1
Danielius Jonušas <danielius.jonusas@gmail.com>
2018-07-16 10:57:24 +0300

    Do not allow multiple click listeners on same view.

--- library/src/main/java/com/djonus/recyclerclick/RecyclerClick.kt
+++ library/src/main/java/com/djonus/recyclerclick/RecyclerClick.kt
@@ -4,29 +4,26 @@ import android.support.annotation.IdRes
 import android.support.v7.widget.RecyclerView
 import android.view.View
 
-private const val TAG_EMITTER = 132645284642.toInt()
-
-private val defaultClickListener = View.OnClickListener {
-    val click = ItemClick(it, it.positionInRecycler())
-    it.getClickListeners().forEach { it(click) }
-}
-
 fun RecyclerView.clicks(@IdRes viewId: Int, listener: (ItemClick) -> Unit) {
-    setClickListeners(viewId, listener)
+    val onClickListener = View.OnClickListener {
+        val click = ItemClick(it, it.positionInRecycler())
+        listener(click)
+    }
+
+    setClickListeners(viewId, onClickListener)
     addOnScrollListener(object : RecyclerView.OnScrollListener() {
         override fun onScrollStateChanged(recyclerView: RecyclerView?, newState: Int) {
             if (newState == RecyclerView.SCROLL_STATE_IDLE) {
-                recyclerView?.setClickListeners(viewId, listener)
+                recyclerView?.setClickListeners(viewId, onClickListener)
             }
         }
     })
 }
 
-private fun RecyclerView.setClickListeners(@IdRes viewId: Int, listener: (ItemClick) -> Unit) {
+private fun RecyclerView.setClickListeners(@IdRes viewId: Int, listener: View.OnClickListener) {
     for (i in 0 until childCount) {
         getChildAt(i).withId(viewId)?.apply {
-            addClickListener(listener)
-            setOnClickListener(defaultClickListener)
+            setOnClickListener(listener)
         }
     }
 }
@@ -36,12 +33,6 @@ private fun View.positionInRecycler(): Int = if (parent is RecyclerView) {
 } else {
     (parent as View).positionInRecycler()
 }
-
-private fun View.addClickListener(emitter: (ItemClick) -> Unit) {
-    setTag(TAG_EMITTER, getClickListeners().plus(emitter))
-}
-
-private fun View.getClickListeners() = getTag(TAG_EMITTER) as? Set<(ItemClick) ->Unit> ?: emptySet()
 
 private fun View.withId(@IdRes viewId: Int): View? = if (id == viewId) this else findViewById(viewId)
 
